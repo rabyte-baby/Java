1e53c483b8ba9654401235f5798d4898f537da53
Aliah Cristine Ariola <aliahcristineariola@gmail.com>
2026-06-25 22:47:57 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,12 @@
+public class KKK {
+    public static void main (String[] args) {
+        
+        double pounds = 123.459d;
+        double inches = 62.59843d;
+        double InchesTimes = inches * inches;
+        double PoundsDivide = pounds / InchesTimes;
+        double BMI = PoundsDivide * 703;
+        
+        System.out.print(BMI);
+    }
+}
